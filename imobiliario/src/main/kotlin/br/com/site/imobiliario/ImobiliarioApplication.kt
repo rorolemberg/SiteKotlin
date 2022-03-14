693fa0502dbe37bbafd9c7056b5304a06b2e0727
Rolemberg <rodrigo_rolemberg@hotmail.com>
2022-03-14 19:28:19 -0300

    primeiro commit

---
+++ imobiliario/src/main/kotlin/br/com/site/imobiliario/ImobiliarioApplication.kt
@@ -0,0 +1,11 @@
+package br.com.site.imobiliario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImobiliarioApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImobiliarioApplication>(*args)
+}
---
+++ imobiliario/src/test/kotlin/br/com/site/imobiliario/ImobiliarioApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.site.imobiliario
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImobiliarioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
